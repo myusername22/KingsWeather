457084ce7debab0fed61589258623ce8ceded7a3
myusername22 <JmacomberBCTHS@yahoo.com>
2018-05-29 20:20:14 -0400

    Initial commit

---
+++ src/main/java/kingsWeather/KingsWeather.java
@@ -0,0 +1,18 @@
+package kingsWeather;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KingsWeather extends JavaPlugin {
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//}
+
+}
